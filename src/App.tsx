18231e6cc07db5ec96bf899f39704ed9544ae2a7
chamoddulanjana <chamodperera128@gmail.com>
2025-06-17 23:31:44 +0530

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
-    <div className='bg-indigo-300'>
-        hello world
+    <div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div>Home Page</div>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
