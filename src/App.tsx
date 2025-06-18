a062d3074fa9ac6fca76de0a567e15dfa4fad74e
chamoddulanjana <chamodperera128@gmail.com>
2025-06-18 12:45:42 +0530

    install hero-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Layout from "@/pages/layout.tsx";
 import Dashboard from "@/app/dashboard/page.tsx";
+import {HeroUIProvider, ToastProvider} from "@heroui/react";
 
 function App() {
 
   return (
     <div>
         <BrowserRouter>
-            <Routes>
-                {/* Redirect from "/" to "/admin-panel" */}
-                <Route path="/" element={<Navigate to="/admin-panel" replace />} />
+            <HeroUIProvider>
+                <ToastProvider/>
+                <Routes>
+                    {/* Redirect from "/" to "/admin-panel" */}
+                    <Route path="/" element={<Navigate to="/admin-panel" replace />} />
 
-                {/* Admin panel layout and nested routes */}
-                <Route path="/admin-panel" element={<Layout />}>
-                    <Route index element={<Navigate to="dashboard" replace />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="products" element={<div />} />
-                </Route>
-            </Routes>
+                    {/* Admin panel layout and nested routes */}
+                    <Route path="/admin-panel" element={<Layout />}>
+                        <Route index element={<Navigate to="dashboard" replace />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="products" element={<div />} />
+                    </Route>
+                </Routes>
+            </HeroUIProvider>
         </BrowserRouter>
     </div>
   )
