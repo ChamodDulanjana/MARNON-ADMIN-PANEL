ca745317afc77fbd9e886f2e7185d609ef77ec8d
chamoddulanjana <chamodperera128@gmail.com>
2025-06-18 11:54:45 +0530

    implementing button for exit admin-panel

--- src/layouts/side-navigation/page.tsx
+++ src/layouts/side-navigation/page.tsx
@@ -1,6 +1,6 @@
 import {
     Sidebar,
-    SidebarContent,
+    SidebarContent, SidebarFooter,
     SidebarGroup,
     SidebarGroupContent,
     SidebarGroupLabel,
@@ -9,6 +9,9 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { menus} from "@/assets/data/menus.ts"
+import { TbArrowBackUp } from "react-icons/tb";
+
+const VITE_WEB_URL: string = import.meta.env.VITE_WEB_URL ?? '';
 
 const SideNavigation = () => {
     return (
@@ -23,9 +26,9 @@ const SideNavigation = () => {
                                     <SidebarMenuItem key={item.title}>
                                         <SidebarMenuButton asChild>
                                             <a href={item.url}>
-                            <span>
-                                <item.icon className="text-lg"/>
-                            </span>
+                                                <span>
+                                                    <item.icon className="text-lg"/>
+                                                </span>
                                                 <span className="text-[14px]">{item.title}</span>
                                             </a>
                                         </SidebarMenuButton>
@@ -35,6 +38,16 @@ const SideNavigation = () => {
                         </SidebarGroupContent>
                     </SidebarGroup>
                 </SidebarContent>
+                <SidebarFooter className='bg-black'>
+                    <div className="pb-4">
+                        <a href={VITE_WEB_URL}  rel="noopener noreferrer">
+                            <div className='flex gap-2 ml-1 text-white hover:bg-white hover:text-red-600 py-2 pl-2 rounded-md transition-colors duration-200'>
+                                <TbArrowBackUp className='mt-[1px] text-lg'/>
+                                <span className="text-[14px]">Exit Admin Panel</span>
+                            </div>
+                        </a>
+                    </div>
+                </SidebarFooter>
             </Sidebar>
         </div>
     );
