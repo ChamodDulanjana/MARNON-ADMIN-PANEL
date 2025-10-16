cd4fb7de80fcd7274913c78aa4180787d54d37e5
chamoddulanjana <chamodperera128@gmail.com>
2025-10-16 23:47:35 +0530

    Add search input and new button

--- src/app/size-management/view-size/page.tsx
+++ src/app/size-management/view-size/page.tsx
@@ -1,8 +1,16 @@
+import {Input, Button} from "@heroui/react";
+import { IoMdAddCircleOutline } from "react-icons/io";
 
 const ViewSize = () => {
     return (
-        <div>
-
+        <div className="w-full">
+            <div className="w-full flex justify-between">
+                <Input type="search" placeholder="Search here..." className="w-2/3 md:w-1/2 lg:w-1/3"/>
+                <Button className="bg-blue-500 text-white font-semibold tracking-wide" >
+                    <IoMdAddCircleOutline className="text-xl -mr-1"/>
+                    New
+                </Button>
+            </div>
         </div>
     );
 };
