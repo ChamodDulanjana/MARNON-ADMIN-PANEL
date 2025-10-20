e4a97072e0b518852e34a26a31733ed5fe23a600
chamoddulanjana <chamodperera128@gmail.com>
2025-10-20 18:18:07 +0530

    create SizeGet and SizeSave DTOs!

---
+++ src/models/size/SizeGetDTO.ts
@@ -0,0 +1,10 @@
+
+export interface SizeGetDTO {
+    id: number;
+    size: string;
+    isActive: boolean;
+    createDate: string;
+    createBy: string;
+    modifyDate: string;
+    modifyBy: string;
+}
