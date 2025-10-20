c152772bc6d9fc892a7090ff50e01b4dd92ec3d7
chamoddulanjana <chamodperera128@gmail.com>
2025-10-20 18:21:02 +0530

    create BottomButtons component!

---
+++ src/components/bottom-buttons/BottomButtons.tsx
@@ -0,0 +1,36 @@
+import {Button} from "@heroui/react";
+import {useNavigate} from "react-router-dom";
+import { FiSave } from "react-icons/fi";
+import { TbArrowBackUp } from "react-icons/tb";
+
+interface Props {
+    backUrl: string;
+}
+
+const BottomButtons = ({backUrl}: Props) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className='w-full flex justify-end gap-4 mt-10'>
+
+            <Button
+                onPress={() => navigate(`${backUrl}`)}
+                className="bg-gray-200 text-gray-700 font-semibold tracking-wide"
+            >
+                <TbArrowBackUp className="-mr-1"/>
+                Back
+            </Button>
+
+            <Button
+                type="submit"
+                className="bg-blue-600 text-white font-semibold tracking-wide"
+            >
+                Save
+                <FiSave className="mt-1 -ml-1"/>
+            </Button>
+
+        </div>
+    );
+};
+
+export default BottomButtons;
