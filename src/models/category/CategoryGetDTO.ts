6f5c457d37f47eefa35d655d94b2d73d5788ca37
chamoddulanjana <chamodperera128@gmail.com>
2025-10-21 08:41:43 +0530

    implementing CategoryDTOs!

---
+++ src/models/category/CategoryGetDTO.ts
@@ -0,0 +1,10 @@
+
+export interface CategoryGetDTO {
+    id: number;
+    name: string;
+    isActive: boolean;
+    createDate: string;
+    createBy: string;
+    modifyDate: string;
+    modifyBy: string;
+}
---
+++ src/models/category/CategorySaveDTO.ts
@@ -0,0 +1,4 @@
+
+export interface CategorySaveDTO {
+    name: string;
+}
